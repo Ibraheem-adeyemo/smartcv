8222cd6119c08d1543a0f9ee2f1ce6aa89e95b5f
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-14 16:32:43 +0100

    fixed build error

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ npm --proxy http://172.25.30.117:6060 install
 
 npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/react
 npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/node
+npm --proxy http://172.25.30.117:6060 install --save-dev eslint
