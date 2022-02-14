2ba274967d1bad53b60f201f514c43636a5519a0
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-14 15:47:09 +0100

    Build error fix

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ npm config set proxy http://172.25.20.117:6060
 npm --proxy http://172.25.30.117:6060 install
 
 npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/react
+npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/node
