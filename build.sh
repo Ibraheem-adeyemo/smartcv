8dc22c03788d63366b6b4e65cf59a5c02b94f144
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-14 17:19:19 +0100

    Build error fix

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ npm --proxy http://172.25.30.117:6060 install
 npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/react
 npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/node
 npm --proxy http://172.25.30.117:6060 install --save-dev eslint
+
+docker-compose up -d
+
+docker-compose push paasui
