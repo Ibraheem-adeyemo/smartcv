3810863d29322940b0c6376127c3d7275dc4d9cc
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-16 15:42:25 +0100

    buid fix

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # npm config set proxy http://172.25.20.117:6060
 
-# npm --proxy $(https_proxy) install
+npm --proxy $(https_proxy) install
 
-# npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/react
-# npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/node
-# npm --proxy http://172.25.30.117:6060 install --save-dev eslint
-# npm --proxy http://172.25.30.117:6060 install --save-dev eslint-config-next
+npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/react
+npm --proxy http://172.25.30.117:6060 install --save-dev typescript @types/node
+npm --proxy http://172.25.30.117:6060 install --save-dev eslint
+npm --proxy http://172.25.30.117:6060 install --save-dev eslint-config-next
 
 docker-compose up -d
 
