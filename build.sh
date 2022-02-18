a9870594d694767d0b2aa6f28b0dfef00b1b88a5
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-18 11:35:32 +0100

    build error fix

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 # npm config set proxy http://172.25.20.117:6060
 
-npm --proxy ${https_proxy} install
+npm --proxy=${http_proxy} install --frozen-lockfile
 
-npm --proxy http://172.25.30.117:6060 install --save-dev typescript@4.4.3 @types/react@17.0.27
-npm --proxy http://172.25.30.117:6060 install --save-dev @types/node
-npm --proxy http://172.25.30.117:6060 install --save-dev eslint@7.32.0
-npm --proxy http://172.25.30.117:6060 install --save-dev eslint-config-next@7.32.0
+npm run build
 
 docker-compose up -d
 
