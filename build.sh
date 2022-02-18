dbe64e5ce649552996b0092e618306738e7a30c6
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-18 11:51:14 +0100

    build error fix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # npm config set proxy http://172.25.20.117:6060
 
-npm --proxy= http://172.25.20.117:6060 install --frozen-lockfile
+npm --proxy=http://172.25.20.117:6060 install --frozen-lockfile
 
 npm run build
 
