cb9e080280024c77af0609c2def06c65961d583c
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-18 15:36:05 +0100

    build error fix

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 npm config set proxy http://172.25.20.117:80
 
-npm install --save-dev typescript@4.4.3
-npm install --save-dev @types/react@17.0.27
-npm install --save-dev @types/node
-npm install --save-dev eslint@7.32.0
-npm install --save-dev eslint-config-next@11.1.2
-
 npm install
+
+npm install --save-dev typescript
+npm install --save-dev @types/react
+npm install --save-dev @types/node
+npm install --save-dev eslint
+npm install --save-dev eslint-config-next
+
 
 npm run build
 
