7103d2d1053d7a31af7888caa9525347f2147df1
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-02-18 15:13:18 +0100

    build error fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ npm install --save-dev typescript@4.4.3
 npm install --save-dev @types/react@17.0.27
 npm install --save-dev @types/node
 npm install --save-dev eslint@7.32.0
-npm install --save-dev eslint-config-next@7.32.0
+npm install --save-dev eslint-config-next@11.1.2
 
 npm install
 
