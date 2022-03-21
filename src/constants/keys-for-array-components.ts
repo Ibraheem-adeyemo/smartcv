2cb230114cfac94ccb9c654523d6e576a792b164
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-03-21 15:08:58 +0100

    code refactoring

---
+++ src/constants/keys-for-array-components.ts
@@ -0,0 +1,8 @@
+export enum keysForArrayComponents {
+    menuText = 'menu-text',
+    missionStatementMotionText= 'mission-statement-motion-text',
+    skeletonLoader = 'skeloader-loader',
+    flexSkeletonLoader = 'flex-skeloader-loader',
+    tableFooter = 'table-footer',
+    tableColumn = 'table-column'
+}
