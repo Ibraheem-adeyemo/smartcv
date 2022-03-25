297b24aae789e0847f6ee8e41ee132c80d3a8beb
Joshua Akande <Joshua.Akande@Interswitchng.com>
2022-03-25 16:26:11 +0100

    text description clarification

--- src/component/app/app-calendar.tsx
+++ src/component/app/app-calendar.tsx
@@ -1,16 +1,35 @@
+import { FC, useState } from "react"
 
 type selectionModeType = "pick-date" | "pick-date-time"
+type getSelectedDateFunc = () =>({date: string, time: string})
 interface AppCalendarProps {
     selectedDate: string,
     selectionMode: selectionModeType
+    getSelectedDate: getSelectedDateFunc
 }
 
-export default function AppCalendar(props:any) {
-
-
-    return <></>
+/**
+ * 
+ * @param {string} date - yyyy-mm-dd
+ */
+function getCalenderDate (date: string) {
+    const sDate = new Date(date)
+    const cArray: number[][] = []
+    const ld = new Date(sDate.getFullYear(), sDate.getMonth() + 1, 0)
+    for(let i = 1; i <= ld.getDate(); i++) {
+        const f = new Date(sDate.getFullYear(), sDate.getMonth() + 1, i)
+        const day = f.getDay();
+        if(i === 1) {
+            cArray[0][day] = f.getDate()
+            continue;
+        }
+        cArray.length
+    }
 }
 
-function f() {
+const AppCalendar:FC<AppCalendarProps> = (props:AppCalendarProps) => {
+    const [selectedDate, setSelectedDate] = useState(props.selectedDate)
+    return (<></>)
+}
 
-} 
+export default AppCalendar
